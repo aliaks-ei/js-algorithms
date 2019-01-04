3fc968a606515ca39268d18e5d997494771280df
alexmozheyko <aleksmozheiko94@gmail.com>
2019-01-04 12:05:35 +0300

    Write tests on Bubble Sort algorithm

---
+++ algorithms/bubble-sort/bubble-sort.md
@@ -0,0 +1,10 @@
+# Bubble sort algorithm
+
+**_bubbleSort(array)_** should return array, sorted with _bubble sort_.
+
+For example:
+
+```
+bubbleSort([5, 3, 8, 2, 1, 4])   //  [1, 2, 3, 4, 5, 8]
+bubbleSort([-7, 13, 4, 0, -15])  //  [-15, -7, 0, 4, 13]
+```
---
+++ algorithms/bubble-sort/bubble-sort.spec.js
@@ -0,0 +1,17 @@
+const bubbleSort = require('./bubble-sort');
+
+describe('Bubble sort: ', () => {
+    test('should return sort array (only positive numbers)', () => {
+        let rawArray    = [5, 3, 8, 2, 1, 4];
+        let sortedArray = [1, 2, 3, 4, 5, 8];
+    
+        expect(bubbleSort(rawArray)).toEqual(sortedArray);
+    });
+    
+    test('should return sort array (positive and negative numbers)', () => {
+        let rawArray    = [-7, 13, 4, 1e8, 0, -15];
+        let sortedArray = [-15, -7, 0, 4, 13, 1e8];
+    
+        expect(bubbleSort(rawArray)).toEqual(sortedArray);
+    });
+});
