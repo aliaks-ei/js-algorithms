703d2a452cd4f7dcd95f7cf75e4dd078c631c1a8
alexmozheyko <aleksmozheiko94@gmail.com>
2019-01-04 12:06:52 +0300

    Update fizz-buzz.spec.js

--- algorithms/fizz-buzz/fizz-buzz.spec.js
+++ algorithms/fizz-buzz/fizz-buzz.spec.js
@@ -1,19 +1,21 @@
 const fizzBuzz = require('./fizz-buzz');
 
-test('tests fizzBuzz with 2 as given number', () => {
-    let expected = [1, 2];
-    let received = fizzBuzz(2);
-
-    expect(received).toEqual(expected);
-});
-
-test('tests fizzBuzz with 15 as given number', () => {
-    let expected = [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"];
-    let received = fizzBuzz(15);
-
-    expect(received).toEqual(expected);
-});
-
-test('tests fizzBuzz with no number passed', () => {
-    expect(fizzBuzz()).toEqual([]);
+describe('Fizz Buzz:', () => {
+    test('tests with 2 as given number', () => {
+        let expected = [1, 2];
+        let received = fizzBuzz(2);
+    
+        expect(received).toEqual(expected);
+    });
+    
+    test('tests with 15 as given number', () => {
+        let expected = [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"];
+        let received = fizzBuzz(15);
+    
+        expect(received).toEqual(expected);
+    });
+    
+    test('tests with no number passed', () => {
+        expect(fizzBuzz()).toEqual([]);
+    });
 });
