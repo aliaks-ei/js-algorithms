382ccb9a29eda2344563aaf3a60c3f4e9d1b8871
alexmozheyko <aleksmozheiko94@gmail.com>
2019-01-08 16:57:34 +0300

    Write test suites on Seive of Eratosthenes algorithm

---
+++ algorithms/seive-of-eratosthenes/README.md
@@ -0,0 +1,15 @@
+# Seive of eratosthenes
+
+## Description
+
+The _Sieve of Eratosthenes_ is a simple algorithm for finding all of the [prime numbers](https://en.wikipedia.org/wiki/Prime_number) up to a certain limit (or between two numbers).  
+
+## Implementation
+
+**_sieveOfEratosthenes(num)_** should return an array of all prime numbers up to _num_.
+
+For example:
+
+```
+sieveOfEratosthenes(20) // [2, 3, 5, 7, 11, 13, 17, 19]
+```
---
+++ algorithms/seive-of-eratosthenes/tests/seive-of-eratosthenes.spec.js
@@ -0,0 +1,23 @@
+const sieveOfEratosthenes = require('../seive-of-eratosthenes');
+
+describe('Seive of Eratosthenes:', () => {
+    test('should return all prime numbers up to 13', () => {
+        let primeNumbersArray = [2, 3, 5, 7, 11, 13];
+
+        expect(sieveOfEratosthenes(13)).toEqual(primeNumbersArray);
+    });
+
+    test('should return all prime numbers up to 27', () => {
+        let primeNumbersArray = [2, 3, 5, 7, 11, 13, 17, 19, 23];
+
+        expect(sieveOfEratosthenes(27)).toEqual(primeNumbersArray);
+    });
+
+    test('should return empty array if no number passed', () => {
+        expect(sieveOfEratosthenes()).toEqual([]);
+    });
+
+    test('should return empty array if negative number passed', () => {
+        expect(sieveOfEratosthenes(-5)).toEqual([]);
+    });
+});
