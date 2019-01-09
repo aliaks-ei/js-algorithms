bb23f771507c517d9c5d2c5ca0139fa8eb63a0a7
alexmozheyko <aleksmozheiko94@gmail.com>
2019-01-09 10:41:44 +0300

    Update Fizz Buzz algorithm and test suites for it

--- algorithms/fizz-buzz/tests/fizz-buzz.spec.js
+++ algorithms/fizz-buzz/tests/fizz-buzz.spec.js
@@ -1,21 +1,21 @@
 const fizzBuzz = require('../fizz-buzz');
 
-describe('Fizz Buzz:', () => {
+describe('Fizz Buzz: ', () => {
     test('tests with 2 as given number', () => {
         let expected = [1, 2];
-        let received = fizzBuzz(2);
+        let number   = 2;
     
-        expect(received).toEqual(expected);
+        expect(fizzBuzz(number)).toEqual(expected);
     });
     
     test('tests with 15 as given number', () => {
         let expected = [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"];
-        let received = fizzBuzz(15);
+        let number   = 15;
     
-        expect(received).toEqual(expected);
+        expect(fizzBuzz(number)).toEqual(expected);
     });
     
-    test('tests with no number passed', () => {
+    test('should return an empty array if no parameter passed', () => {
         expect(fizzBuzz()).toEqual([]);
     });
 });
