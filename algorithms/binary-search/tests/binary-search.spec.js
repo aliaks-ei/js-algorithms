b77688a499289025accf5e9da87d8d58e76f14d2
alexmozheyko <aleksmozheiko94@gmail.com>
2019-01-09 10:29:07 +0300

    Update test suites for Binary search algorithm

--- algorithms/binary-search/tests/binary-search.spec.js
+++ algorithms/binary-search/tests/binary-search.spec.js
@@ -1,21 +1,21 @@
 const binarySearch = require('../binary-search');
 
 describe('Binary search: ', () => {
-    test('should return true', () => {
+    test('should return true (array contains key)', () => {
         let numArray = [5, 7, 12, 16, 36, 56, 71];
         let key      = 56;
     
         expect(binarySearch(numArray, key)).toEqual(true);
     });
     
-    test('should return false', () => {
+    test('should return false (array does not contains key)', () => {
         let numArray = [5, 7, 12, 16, 36, 56, 71];
         let key      = 99;
     
         expect(binarySearch(numArray, key)).toEqual(false);
     });
 
-    test('should return false', () => {
+    test('should return false (empty array passed)', () => {
         let numArray = [];
         let key      = -33;
     
